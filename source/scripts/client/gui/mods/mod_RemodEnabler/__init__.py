4cba90a15e49eefa7f565127fb5ef332d32953cc
PolyacovYury <kuf-1@yandex.ru>
2017-06-30 19:14:24 +0300

    RemodEnabler: processor loads now

--- source/scripts/client/gui/mods/mod_RemodEnabler/__init__.py
+++ source/scripts/client/gui/mods/mod_RemodEnabler/__init__.py
@@ -1,6 +1,8 @@
 import PYmodsCore
 from config import g_config
-
+import skinLoader
+import processor
+import collision
 
 g_config.load()
 statistic_mod = PYmodsCore.Analytics(g_config.ID, g_config.version.split(' ', 1)[0], 'UA-76792179-4')
