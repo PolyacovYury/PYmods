28218e47655b40a9f75fea6d0714bcb0b1ba33bd
PolyacovYury <kuf-1@yandex.ru>
2017-08-14 03:19:48 +0300

    SoundEventInjector: added gun reload effects

--- source/scripts/client/gui/mods/mod_SoundEventInjector.py
+++ source/scripts/client/gui/mods/mod_SoundEventInjector.py
@@ -5,6 +5,7 @@ import items.vehicles
 import nations
 import os
 import traceback
+from ReloadEffect import _BarrelReloadDesc
 from debug_utils import LOG_ERROR
 from items.components import sound_components
 
@@ -13,7 +14,7 @@ class _Config(PYmodsCore.Config):
     def __init__(self):
         super(self.__class__, self).__init__('%(mod_ID)s')
         self.version = '1.0.0 (%(file_compile_date)s)'
-        self.data = {'engines': {}, '': {}}
+        self.data = {'engines': {}, 'gunReloadEffects': {}}
 
     def updateMod(self):
         pass
@@ -34,13 +35,17 @@ class _Config(PYmodsCore.Config):
                 if itemType not in self.data:
                     continue
                 items = self.data[itemType]
-                for nationName, nationData in itemsData.iteritems():
-                    if nationName not in nations.NAMES:
-                        print '%s: unknown nation in %s data: %s' % (self.ID, itemType, nationName)
-                        continue
-                    items[nationName] = {}
-                    for itemName in nationData:
-                        items[nationName][itemName] = nationData[itemName]
+                if itemType in ('engines',):
+                    for nationName, nationData in itemsData.iteritems():
+                        if nationName not in nations.NAMES:
+                            print '%s: unknown nation in %s data: %s' % (self.ID, itemType, nationName)
+                            continue
+                        items[nationName] = {}
+                        for itemName in nationData:
+                            items[nationName][itemName] = nationData[itemName]
+                if itemType in ('gunReloadEffects',):
+                    for itemName in itemsData:
+                        items[itemName] = itemsData[itemName]
 
     def load(self):
         self.update_data(True)
@@ -62,7 +67,34 @@ def new_readEngine(base, xmlCtx, section, item, *args):
         return
     sounds = item.sounds
     itemData = engines[item.name]
-    item.sounds = sound_components.WWTripleSoundConfig(sounds.wwsound, itemData.get('wwsoundPC', sounds.wwsoundPC), itemData.get('wwsoundNPC', sounds.wwsoundNPC))
+    item.sounds = sound_components.WWTripleSoundConfig(sounds.wwsound, itemData.get('wwsoundPC', sounds.wwsoundPC),
+                                                       itemData.get('wwsoundNPC', sounds.wwsoundNPC))
+
+
+@PYmodsCore.overrideMethod(items.vehicles, '_readReloadEffectGroups')
+def new_readReloadEffects(base, xmlPath):
+    res = base(xmlPath)
+    effectsData = _config.data['gunReloadEffects']
+    for sname in res:
+        if sname not in effectsData:
+            continue
+        effData = effectsData[sname]
+        descr = res[sname]
+        descr.duration = float(effData.get('duration', descr.duration * 1000.0)) / 1000.0
+        descr.soundEvent = effData.get('sound', descr.soundEvent)
+        if effData['type'] == 'BarrelReload' and isinstance(descr, _BarrelReloadDesc):
+            descr.lastShellAlert = effData.get('lastShellAlert', descr.lastShellAlert)
+            descr.shellDuration = effData.get('shellDuration', descr.shellDuration * 1000.0) / 1000.0
+            descr.startLong = effData.get('startLong', descr.startLong)
+            descr.startLoop = effData.get('startLoop', descr.startLoop)
+            descr.stopLoop = effData.get('stopLoop', descr.stopLoop)
+            descr.loopShell = effData.get('loopShell', descr.loopShell)
+            descr.loopShellLast = effData.get('loopShellLast', descr.loopShellLast)
+            descr.ammoLow = effData.get('ammoLow', descr.ammoLow)
+            descr.caliber = effData.get('caliber', descr.caliber)
+            descr.shellDt = effData.get('loopShellDt', descr.shellDt)
+            descr.shellDtLast = effData.get('loopShellLastDt', descr.shellDtLast)
+    return res
 
 
 _config = _Config()
