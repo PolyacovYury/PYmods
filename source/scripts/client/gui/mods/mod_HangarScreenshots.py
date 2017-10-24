d12082a9a80c8dc52cd1e454e55e901c5196447d
PolyacovYury <kuf-1@yandex.ru>
2017-10-24 22:28:04 +0300

    HangarScreenshots: added ability to move camera around when UI is hidden

--- source/scripts/client/gui/mods/mod_HangarScreenshots.py
+++ source/scripts/client/gui/mods/mod_HangarScreenshots.py
@@ -5,7 +5,7 @@ import Keys
 import Math
 import PYmodsCore
 import traceback
-from gui import InputHandler
+from gui import InputHandler, SystemMessages
 from gui.ClientHangarSpace import ClientHangarSpace
 from gui.Scaleform.framework import ViewTypes
 from gui.app_loader.loader import g_appLoader
@@ -25,6 +25,8 @@ class _Config(PYmodsCore.Config):
                      'toggleKey': self.defaultKeys['toggleKey'],
                      'camkey': self.defaultKeys['camkey'],
                      'camKey': self.defaultKeys['camKey'],
+                     'lockCamera': True,
+                     'addUnlockMode': True,
                      'cameraPositions': [{'target_pos': [0.0, 1.2, 0.0],
                                           'pivot_pos': [0.0, 1.0, 0.0],
                                           'angles': [20.0, -13.0],
@@ -32,19 +34,28 @@ class _Config(PYmodsCore.Config):
                      'currentCamPos': 0}
         self.i18n = {
             'UI_description': 'Hangar screenshots',
+            'UI_message_cameraUnlocked': 'Camera unlocked.',
             'UI_setting_togglekey_text': 'Mod activation hotkey',
             'UI_setting_togglekey_tooltip': 'Pressing this button in hangar turns hangar UI off and locks the camera.',
             'UI_setting_camkey_text': 'Camera angle change hotkey',
             'UI_setting_camkey_tooltip': 'Pressing this button in hangar while the mod is active changes the camera view '
-                                         'point.'}
+                                         'point.',
+            'UI_setting_lockCamera_text': 'Lock camera when UI is disabled',
+            'UI_setting_lockCamera_tooltip': 'This setting restricts you from moving your camera with a mouse when '
+                                             'hangar UI is turned off.',
+            'UI_setting_addUnlockMode_text': 'Add unlocked camera mode into the list of modes',
+            'UI_setting_addUnlockMode_tooltip': 'This setting adds another camera view "point" into the list which lets '
+                                                'you to move the camera with the mouse when hangar UI is turned off.'}
         self.loadLang()
 
     def template_settings(self):
         return {'modDisplayName': self.i18n['UI_description'],
                 'settingsVersion': 200,
                 'enabled': self.data['enabled'],
-                'column1': [self.createHotKey('togglekey')],
-                'column2': [self.createHotKey('camkey')]}
+                'column1': [self.createHotKey('togglekey'),
+                            self.createControl('lockCamera')],
+                'column2': [self.createHotKey('camkey'),
+                            self.createControl('addUnlockMode')]}
 
 
 _config = _Config()
@@ -59,7 +70,8 @@ def toggleHangarUI(visible):
 
 @PYmodsCore.overrideMethod(ClientHangarSpace, 'updateCameraByMouseMove')
 def new_updateCameraByMouseMove(base, *args):
-    if _config.data['UIVisible'] or not _config.data['cameraPositions']:
+    if _config.data['UIVisible'] or not _config.data['cameraPositions'] or _config.data['currentCamPos'] == len(
+            _config.data['cameraPositions']) or not _config.data['lockCamera']:
         base(*args)
 
 
@@ -78,14 +90,21 @@ def inj_hkKeyEvent(event):
                     if PYmodsCore.checkKeys(_config.data['togglekey']):
                         _config.data['UIVisible'] = not _config.data['UIVisible']
                         toggleHangarUI(_config.data['UIVisible'])
-                        if not _config.data['UIVisible'] and _config.data['cameraPositions']:
+                        if not _config.data['UIVisible'] and _config.data['cameraPositions'] and _config.data[
+                                'currentCamPos'] < len(_config.data['cameraPositions']):
                             setCameraLocation(_config.data['cameraPositions'][_config.data['currentCamPos']])
                     elif PYmodsCore.checkKeys(_config.data['camkey']):
                         if not _config.data['UIVisible'] and _config.data['cameraPositions']:
                             _config.data['currentCamPos'] += 1
-                            if _config.data['currentCamPos'] >= len(_config.data['cameraPositions']):
+                            if _config.data['currentCamPos'] == len(_config.data['cameraPositions']) and _config.data[
+                                    'addUnlockMode'] and _config.data['lockCamera']:
+                                SystemMessages.pushMessage('PYmods_SM' + _config.i18n['UI_message_cameraUnlocked'],
+                                                           SystemMessages.SM_TYPE.Warning)
+                            elif _config.data['currentCamPos'] >= len(_config.data['cameraPositions']) + _config.data[
+                                    'addUnlockMode']:
                                 _config.data['currentCamPos'] = 0
-                            setCameraLocation(_config.data['cameraPositions'][_config.data['currentCamPos']])
+                            else:
+                                setCameraLocation(_config.data['cameraPositions'][_config.data['currentCamPos']])
             elif not _config.data['UIVisible']:
                 _config.data['currentCamPos'] = 0
                 _config.data['UIVisible'] = True
