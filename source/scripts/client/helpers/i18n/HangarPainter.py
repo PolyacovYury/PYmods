b96a24956f2a71b7482e12f52d885305bddd7cff
PolyacovYury <kuf-1@yandex.ru>
2017-11-11 19:06:18 +0300

    HangarPainter: 1.2.0: tankmen code overhaul. cleanColour option removed.

--- source/scripts/client/helpers/i18n/HangarPainter.py
+++ source/scripts/client/helpers/i18n/HangarPainter.py
@@ -2,6 +2,7 @@
 import BigWorld
 import PYmodsCore
 import json
+import re
 import traceback
 import urllib2
 from debug_utils import LOG_ERROR
@@ -14,14 +15,13 @@ def __dir__():
 class _Config(PYmodsCore.Config):
     def __init__(self):
         super(self.__class__, self).__init__('%(mod_ID)s')
-        self.version = '1.1.2 (%(file_compile_date)s)'
+        self.version = '1.2.0 (%(file_compile_date)s)'
         self.data = {'enabled': True,
                      'debug': True,
                      'debugColour': True,
                      'debugBegin': 0,
                      'crewColour': True,
-                     'cleanColour': False,
-                     'colour': '0097FA'}
+                     'colour': '66CC00'}
         self.backupData = {}
         self.i18n = {
             'UI_description': 'Hangar Painter',
@@ -29,25 +29,17 @@ class _Config(PYmodsCore.Config):
             'UI_setting_colour_text': '<font color=\'#%(colour)s\'>Current colour: #%(colour)s</font>',
             'UI_setting_colour_tooltip': (
                 'This colour will be applied to all hangar texts.\n'
-                '\n<b>WARNING!</b> Restart required for this setting to be applied properly.'),
-            'UI_setting_crewColour_text': 'Enable crew texts colouring',
+                '\n<b>WARNING!</b> Restart is required for this setting to be applied properly.'),
+            'UI_setting_crewColour_text': 'Enable crew names colouring',
             'UI_setting_crewColour_tooltip': (
-                'Crew names, ranks and roles will be coloured, but this sometimes '
-                'does not function properly.\n\n<b>WARNING!</b> Restart required for this setting to be applied.'),
-            'UI_setting_cleanColour_text': 'Enable clean mode',
-            'UI_setting_cleanColour_tooltip': (
-                'If disabled, all available texts will be coloured.\nIf enabled, rare cases of "unreadable" texts will be '
-                'fixed, but these texts will not be coloured.\n\n<b>WARNING!</b> Restart required for this setting to be '
-                'applied.'),
+                'Crew names and ranks in the sidebar will be coloured, but it glitches in tankmen replacement '
+                'view.\n\nYou will need to select another vehicle for changes to take effect.\n\nIf you experience '
+                'issues after changing this setting - re-open this settings window.'),
             'UI_restart_header': 'Restart request',
             'UI_restart_text': 'Hangar Painter: {reason}. Client restart required to accept changes.',
             'UI_restart_reason_colourChanged': 'text colour was changed',
             'UI_restart_reason_modDisabled': 'mod was disabled',
             'UI_restart_reason_modEnabled': 'mod was enabled',
-            'UI_restart_reason_crewDisabled': 'crew colouring was disabled',
-            'UI_restart_reason_crewEnabled': 'crew colouring was enabled',
-            'UI_restart_reason_cleanDisabled': 'clean colouring was disabled',
-            'UI_restart_reason_cleanEnabled': 'clean colouring was enabled',
             'UI_restart': 'Restart'}
         self.blacklists = {}
         self.needRestart = False
@@ -62,12 +54,11 @@ class _Config(PYmodsCore.Config):
                 'enabled': self.data['enabled'],
                 'column1': [colourLabel,
                             self.createControl('colour', 'TextInputColor', empty=True)],
-                'column2': [self.createControl('crewColour'),
-                            self.createControl('cleanColour')]}
+                'column2': [self.createControl('crewColour')]}
 
     def apply_settings(self, settings):
         for setting in settings:
-            if setting in ('colour', 'enabled', 'crewColour', 'cleanColour') and setting not in self.backupData:
+            if setting in ('colour', 'enabled') and setting not in self.backupData:
                 self.backupData[setting] = self.data[setting]
 
         super(self.__class__, self).apply_settings(settings)
@@ -75,7 +66,7 @@ class _Config(PYmodsCore.Config):
     def onWindowClose(self):
         if any(self.data[setting] != self.backupData[setting] for setting in self.backupData):
             self.onRequestRestart(self.data[key] != self.backupData.get(key, self.data[key]) for key in
-                                  ('colour', 'enabled', 'crewColour', 'cleanColour'))
+                                  ('colour', 'enabled'))
         self.backupData = {}
 
     def update_data(self, doPrint=False):
@@ -88,18 +79,12 @@ class _Config(PYmodsCore.Config):
         BigWorld.restartGame()
 
     def onRequestRestart(self, reason):
-        colourChanged, toggled, crewChanged, cleanChanged = reason
+        colourChanged, toggled = reason
         reasons = []
         if colourChanged:
             reasons.append(self.i18n['UI_restart_reason_colourChanged'])
         if toggled:
             reasons.append(self.i18n['UI_restart_reason_mod%s' % ('Enabled' if self.data['enabled'] else 'Disabled')])
-        if crewChanged:
-            reasons.append(
-                self.i18n['UI_restart_reason_crew%s' % ('Enabled' if self.data['crewColour'] else 'Disabled')])
-        if cleanChanged:
-            reasons.append(
-                self.i18n['UI_restart_reason_clean%s' % ('Enabled' if self.data['cleanColour'] else 'Disabled')])
         dialogText = self.i18n['UI_restart_text'].format(reason='; '.join(reasons))
         from gui import DialogsInterface
         from gui.Scaleform.daapi.view.dialogs import SimpleDialogMeta, InfoDialogButtons
@@ -144,16 +129,17 @@ def i18n_hook_makeString(key, *args, **kwargs):
                 for listType, idList in _config.blacklists.iteritems()}
             identity['commonBlacklist'] = identity['commonBlacklist'] or (
                 '#messenger' in moFile and subkey.startswith('server/errors/') and subkey.endswith('/title'))
+            if moFile == '#menu' and subkey.startswith('tankmen/') and len(subkey.split('/')) == 2:
+                from CurrentVehicle import g_currentVehicle
+                if g_currentVehicle.isPresent() and g_currentVehicle.item.type in subkey:
+                    identity['commonBlacklist'] = False
             whitelist = _config.blacklists['commonWhitelist']
             identity['commonWhitelist'] = any(
                 moKey in moFile and any(x == subkey for x in whitelist[moKey]) for moKey in whitelist)
-            if not (identity['commonBlacklist'] or
-                    identity['crewBlacklist'] and not _config.data['crewColour'] or
-                    identity['cleanBlacklist'] and not identity['cleanWhitelist'] and _config.data[
-                            'cleanColour']) or identity['commonWhitelist']:
+            if not identity['commonBlacklist'] or identity['commonWhitelist']:
                 if not _config.data['debug']:
                     translation = old_makeString(key, *args, **kwargs)
-                    if translation.strip() and not translation == subkey:
+                    if translation.strip() and translation not in (key, subkey):
                         return "<font color='#%s'>%s</font>" % (_config.data['colour'], translation)
                     else:
                         return translation
@@ -171,4 +157,57 @@ def i18n_hook_makeString(key, *args, **kwargs):
         return old_makeString(key, *args, **kwargs)
 
 
+def new_as_tankmenResponseS(base, self, data):
+    if _config.data['enabled']:
+        from CurrentVehicle import g_currentVehicle
+        vehicle = g_currentVehicle.item
+        for tankmanData in data['tankmen']:
+            for key in tankmanData:
+                if (key in ('firstName', 'lastName', 'rank') and _config.data['crewColour']) or key == 'role' or \
+                        (key == 'vehicleType' and tankmanData[key] == vehicle.shortUserName):
+                    tankmanData[key] = "<font color='#%s'>%s</font>" % (_config.data['colour'], tankmanData[key])
+        for roleData in data['roles']:
+            for key in ('role', 'vehicleType'):
+                roleData[key] = "<font color='#%s'>%s</font>" % (_config.data['colour'], roleData[key])
+    return base(self, data)
+
+
+def new_tankmanSkill_getValue(base, self):
+    result = base(self)
+    if _config.data['enabled']:
+        for skill in result:
+            skill['label'] = "<font color='#%s'>%s</font>" % (_config.data['colour'], skill['label'])
+    return result
+
+
+def new_tankmanAttr_getValue(base, self):
+    result = base(self)
+    return "<font color='#%s'>%s</font>" % (_config.data['colour'], result) if _config.data['enabled'] and self._name in\
+        ('name', 'rank', 'role', 'isInTank', 'efficiencyRoleLevel', 'currentVehicleName') else result
+
+
+def new_I18nDialog_init(base, self, *args):
+    base(self, *args)
+    if _config.data['enabled']:
+        TAG_RE = re.compile(r'<[^>]+>')
+        for key in self._messageCtx:
+            self._messageCtx[key] = TAG_RE.sub('', self._messageCtx[key])
+
+
+def delayedHooks():
+    # noinspection PyGlobalUndefined
+    global new_as_tankmenResponseS, new_tankmanSkill_getValue, new_I18nDialog_init, new_tankmanAttr_getValue
+    from gui.Scaleform.daapi.view.lobby.hangar.Crew import Crew
+    from gui.shared.tooltips.tankman import TankmanSkillListField, ToolTipAttrField, TankmanRoleLevelField, \
+        TankmanCurrentVehicleAttrField
+    from gui.Scaleform.daapi.view.dialogs import I18nDialogMeta
+    PYmodsCore.overrideMethod(Crew, 'as_tankmenResponseS')(new_as_tankmenResponseS)
+    PYmodsCore.overrideMethod(TankmanSkillListField, '_getValue')(new_tankmanSkill_getValue)
+    PYmodsCore.overrideMethod(ToolTipAttrField, '_getValue')(new_tankmanAttr_getValue)
+    PYmodsCore.overrideMethod(TankmanRoleLevelField, '_getValue')(new_tankmanAttr_getValue)
+    PYmodsCore.overrideMethod(TankmanCurrentVehicleAttrField, '_getValue')(new_tankmanAttr_getValue)
+    PYmodsCore.overrideMethod(I18nDialogMeta, '__init__')(new_I18nDialog_init)
+
+
+BigWorld.callback(0, delayedHooks)
 statistic_mod = PYmodsCore.Analytics(_config.ID, _config.version, 'UA-76792179-6')
