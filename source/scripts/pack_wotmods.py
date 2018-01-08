bda96ff64954eabbc68627051f083c20098c48f5
PolyacovYury <kuf-1@yandex.ru>
2018-01-08 21:19:46 +0300

    Automated updating dependencies such as polarfox.vxSettingsApi.*.wotmod.   - options replaced: -x -> -i -e     -i regexp: regular expression for files to include     -e regexp: regular expression for files to exclude   - option added:     -w wildcards: wildcards for files to be identical in archive and in source folder, split by "|". case-insensitive.

--- source/scripts/pack_wotmods.py
+++ source/scripts/pack_wotmods.py
@@ -10,11 +10,11 @@ import zipfile
 def main():
     import getopt
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'lfqm:x:v:c:d:s:')
+        opts, args = getopt.getopt(sys.argv[1:], 'lfqm:i:e:v:c:w:d:s:')
     except getopt.error, msg:
         print msg
-        print "usage: python pack_wotmods.py [-l] [-f] [-q] [-m mode] [-x regexp] [-v version_str] [-c creation_date] [-d " \
-              "destdir] [-s sourcedir] [directory|file ...]"
+        print "usage: python pack_wotmods.py [-l] [-f] [-q] [-m mode] [-i regexp] [-e regexp] [-v version_str] " \
+              "[-c creation_date] [-w wildcards] [-d destdir] [-s sourcedir] [directory|file ...]"
         print
         print "arguments: zero or more file and directory names to repack; if no arguments given,"
         print "           defaults to the equivalent of -l sys.path"
@@ -24,10 +24,13 @@ def main():
         print "-f: force rebuild even if timestamps are up-to-date"
         print "-q: output only error messages"
         print "-m mode: 0 (ZIP_STORED) or 1 (ZIP_DEFLATED)"
-        print "-x regexp: regular expression for files to match"
+        print "-i regexp: regular expression for files to include"
+        print "-e regexp: regular expression for files to exclude"
         print "-v version_str: versioned folder name. If given - all version-like folders (matching " \
               r'regex "mods/[.\d]*/") will be renamed to match "mods/version_str/".'
         print '-c creation_date: "%Y-%m-%d %H:%M:%S" - date of versioned folder creation.'
+        print '-w wildcards: wildcards for files to be identical in archive and in source folder, split by "|".' \
+              ' case-insensitive.'
         print "-d destdir: directory to prepend to file paths for use in wotmod file"
         print "-s sourcedir: directory to pick files from"
         print "directory|file: place to pick wotmod(s) from"
@@ -36,9 +39,11 @@ def main():
     force = 0
     quiet = 0
     mode = zipfile.ZIP_STORED
-    rx = None
+    ix = None
+    ex = None
     version_str = None
     date = None
+    wc = None
     ddir = None
     sdir = None
     for o, a in opts:
@@ -46,9 +51,14 @@ def main():
         if o == '-f': force = 1
         if o == '-q': quiet = 1
         if o == '-m': mode = zipfile.ZIP_STORED if not int(a) else zipfile.ZIP_DEFLATED
-        if o == '-x':
+        if o == '-i':
             import re
-            rx = re.compile(a)
+            ix = re.compile(a)
+        if o == '-e':
+            import re
+            ex = re.compile(a)
+        if o == '-w':
+            wc = a.split('|')
         if o == '-v':
             import re
             if re.match(r'[.\d]*', a):
@@ -66,10 +76,10 @@ def main():
         if args:
             for arg in args:
                 if os.path.isdir(arg):
-                    if not pack_dir(arg, maxlevels, sdir, ddir, version_str, date, rx, force, mode, quiet):
+                    if not pack_dir(arg, maxlevels, sdir, ddir, version_str, date, ix, ex, wc, force, mode, quiet):
                         success = 0
                 else:
-                    if not pack_file(arg, sdir, ddir, version_str, date, rx, force, mode, quiet):
+                    if not pack_file(arg, sdir, ddir, version_str, date, ix, ex, wc, force, mode, quiet):
                         success = 0
         else:
             success = pack_path()
@@ -97,7 +107,8 @@ def pack_path(skip_curdir=1, maxlevels=0, quiet=0):
     return success
 
 
-def pack_dir(dir, maxlevels=10, sdir=None, ddir=None, version_str=None, date=None, rx=None, force=0, mode=zipfile.ZIP_STORED,
+def pack_dir(dir, maxlevels=10, sdir=None, ddir=None, version_str=None, date=None, ix=None, ex=None, wc=None, force=0,
+             mode=zipfile.ZIP_STORED,
              quiet=0):
     """Pack all files in the given directory tree.
 
@@ -122,21 +133,23 @@ def pack_dir(dir, maxlevels=10, sdir=None, ddir=None, version_str=None, date=Non
     for name in names:
         fullname = os.path.join(dir, name).replace(os.sep, '/')
         if not os.path.isdir(fullname):
-            if not pack_file(fullname, sdir, ddir, version_str, date, rx, force, mode, quiet):
+            if not pack_file(fullname, sdir, ddir, version_str, date, ix, ex, wc, force, mode, quiet):
                 success = 0
         elif maxlevels > 0 and name != os.curdir and name != os.pardir and os.path.isdir(fullname) and not os.path.islink(
                 fullname):
-            if not pack_dir(fullname, maxlevels - 1, sdir, ddir, version_str, date, rx, force, mode, quiet):
+            if not pack_dir(fullname, maxlevels - 1, sdir, ddir, version_str, date, ix, ex, wc, force, mode, quiet):
                 success = 0
     return success
 
 
-def pack_file(fullname, sdir=None, ddir=None, version_str=None, date=None, rx=None, force=0, mode=zipfile.ZIP_STORED,
-              quiet=0):
+def pack_file(fullname, sdir=None, ddir=None, version_str=None, date=None, ix=None, ex=None, wc=None, force=0,
+              mode=zipfile.ZIP_STORED, quiet=0):
     success = 1
-    if rx is not None:
-        mo = rx.search(fullname)
-        if not mo:
+    if ix is not None:
+        if not ix.search(fullname):
+            return success
+    if ex is not None:
+        if ex.search(fullname):
             return success
     if not quiet:
         print 'Checking', fullname, '...'
@@ -146,21 +159,42 @@ def pack_file(fullname, sdir=None, ddir=None, version_str=None, date=None, rx=No
             orig_folder = ''
             if version_str:
                 import re
-                folder_rx = re.compile('mods/[.\d]*/$')
-                folder_rx_all = re.compile('mods/[.\d]*/')
+                folder_ix = re.compile('mods/[.\d]*/$')
+                folder_ix_all = re.compile('mods/[.\d]*/')
                 for info in orig_infos_full:
-                    if folder_rx_all.search(info.filename) and 'mods/%s/' % version_str not in info.filename:
-                        search = folder_rx.search(info.filename)
+                    filename = info.filename
+                    if folder_ix_all.search(filename) and 'mods/%s/' % version_str not in filename:
+                        search = folder_ix.search(filename)
                         if search:
                             info.date_time = date
                             orig_folder = search.group()
                             if quiet:
                                 print 'Checking', fullname, '...'
-                            print 'Updating versioned folder:', info.filename
-                        info.filename = folder_rx_all.sub('mods/%s/' % version_str, info.filename)
-            orig_infos = [x for x in orig_infos_full if
+                            print 'Updating versioned folder:', filename
+                        info.filename = folder_ix_all.sub('mods/%s/' % version_str, filename)
+            replaced = {}
+            if wc:
+                import fnmatch
+                import glob
+                for card in wc:
+                    for info in orig_infos_full:
+                        dfile = info.filename
+                        sfile = dfile.replace(ddir, sdir)
+                        card_glob = [x.replace(os.sep, '/') for x in glob.iglob('%s/%s' % (os.path.dirname(sfile), card))]
+                        if fnmatch.fnmatch(dfile, card) and not os.path.exists(sfile) and card_glob:
+                            if len(card_glob) > 1:
+                                print 'Ambiguous wildcard:', card, 'picked file:', card_glob[0], 'other matches:', \
+                                    card_glob[1:]
+                            if dfile in replaced.values():
+                                print 'Intersecting wildcards match with file', dfile, 'detected at:', card
+                            else:
+                                new_file = card_glob[0].replace(sdir, ddir)
+                                replaced[new_file] = dfile
+                                info.filename = new_file
+                                info.date_time = datetime.fromtimestamp(os.stat(card_glob[0]).st_mtime).timetuple()[:6]
+            orig_infos = [x for x in orig_infos_full if x.filename in replaced or
                           x.filename.startswith(ddir) and os.path.exists(x.filename.replace(ddir, sdir))]
-            if not force and not orig_folder and all(
+            if not force and not orig_folder and not replaced and all(
                     os.stat(x.filename.replace(ddir, sdir)).st_size == x.file_size for x in orig_infos if
                     not x.filename.endswith('/')):
                 return success
