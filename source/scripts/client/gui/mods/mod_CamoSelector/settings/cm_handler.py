d2aa44fa6f417cb5fc4456a927e9a4586a2e05ef
PolyacovYury <kuf-1@yandex.ru>
2018-02-03 16:39:16 +0300

    Oversimplified...

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/cm_handler.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/cm_handler.py
@@ -1,11 +1,23 @@
 from PYmodsCore import overrideMethod
+from gui.Scaleform import MENU
 from gui.Scaleform.daapi.view.lobby.customization import CustomizationItemCMHandler
+from gui.Scaleform.daapi.view.lobby.customization.customization_cm_handlers import CustomizationOptions
 from gui.Scaleform.daapi.view.lobby.customization.main_view import MainView
+from items.components.c11n_constants import SeasonType
 
 
 @overrideMethod(CustomizationItemCMHandler, '_generateOptions')
 def _generateOptions(base, self, ctx=None):
     """ Generate a list of options for  the context menu.
     """
-    result = base(self, ctx)
-    return [result[-1]] if not isinstance(self._c11nView, MainView) else result
+    if isinstance(self._c11nView, MainView):
+        return base(self, ctx)
+    item = self.itemsCache.items.getItemByCD(self._intCD)
+    removeFromTankEnabled = False
+    for outfit in (self._c11nView.getModifiedOutfit(season) for season in SeasonType.COMMON_SEASONS):
+        if outfit.has(item):
+            removeFromTankEnabled = True
+            break
+    return [
+        self._makeItem(CustomizationOptions.REMOVE_FROM_TANK, MENU.cst_item_ctx_menu(CustomizationOptions.REMOVE_FROM_TANK),
+                       {'enabled': removeFromTankEnabled})]
