1e484b80f954e1b911b79b768b1efb5bb2617365
PolyacovYury <kuf-1@yandex.ru>
2018-02-03 15:41:27 +0300

    Further improvements.

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/cm_handler.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/cm_handler.py
@@ -1,14 +1,6 @@
 from PYmodsCore import overrideMethod
 from gui.Scaleform.daapi.view.lobby.customization import CustomizationItemCMHandler
-from gui.Scaleform.daapi.view.lobby.customization.customization_cm_handlers import CustomizationOptions
 from gui.Scaleform.daapi.view.lobby.customization.main_view import MainView
-
-
-@overrideMethod(CustomizationItemCMHandler, '__init__')
-def __init__(base, self, cmProxy, ctx=None):
-    base(self, cmProxy, ctx)
-    if not isinstance(self._c11nView, MainView):
-        self._CustomizationItemCMHandler__handlers = {CustomizationOptions.REMOVE_FROM_TANK: 'removeItemFromTank'}
 
 
 @overrideMethod(CustomizationItemCMHandler, '_generateOptions')
@@ -16,4 +8,4 @@ def _generateOptions(base, self, ctx=None):
     """ Generate a list of options for  the context menu.
     """
     result = base(self, ctx)
-    return [result[-1]] if isinstance(self._c11nView, MainView) else result
+    return [result[-1]] if not isinstance(self._c11nView, MainView) else result
