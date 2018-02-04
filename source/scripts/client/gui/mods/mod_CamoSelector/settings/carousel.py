7aa61c8681dfba1dd7f500f55f9e2fc3d212500c
PolyacovYury <kuf-1@yandex.ru>
2018-02-04 12:31:32 +0300

    Properly getting and sorting the camouflages

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/carousel.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/carousel.py
@@ -1,5 +1,6 @@
-from gui.Scaleform.daapi.view.lobby.customization.customization_carousel import CustomizationSeasonAndTypeFilterData, \
-    comparisonKey, CustomizationBookmarkVO
+import os
+from gui.Scaleform.daapi.view.lobby.customization.customization_carousel import CustomizationBookmarkVO, \
+    CustomizationSeasonAndTypeFilterData, comparisonKey
 from gui.Scaleform.framework.entities.DAAPIDataProvider import SortableDAAPIDataProvider
 from gui.Scaleform.locale.VEHICLE_CUSTOMIZATION import VEHICLE_CUSTOMIZATION
 from gui.shared.gui_items import GUI_ITEM_TYPE
@@ -7,10 +8,12 @@ from gui.shared.utils.requesters import REQ_CRITERIA
 from helpers import dependency
 from helpers.i18n import makeString as _ms
 from items.components.c11n_constants import SeasonType
+from items.vehicles import g_cache
 from skeletons.gui.customization import ICustomizationService
 from skeletons.gui.server_events import IEventsCache
 from skeletons.gui.shared import IItemsCache
 from .shared import CUSTOMIZATION_TABS
+from .. import g_config
 
 
 class CustomizationCarouselDataProvider(SortableDAAPIDataProvider):
@@ -32,15 +35,30 @@ class CustomizationCarouselDataProvider(SortableDAAPIDataProvider):
         self._currentlyApplied = set()
         self._allSeasonAndTabFilterData = {}
         requirement = self._createBaseRequirements()
-        allItems = self.itemsCache.items.getItems(GUI_ITEM_TYPE.CAMOUFLAGE, requirement)
+        camouflages = g_cache.customization20().camouflages.values()
+        allItems = [item for item in (self.itemsCache.items.getItemByCD(camo.compactDescr) for camo in camouflages)
+                    if requirement(item)]
         for tabIndex in CUSTOMIZATION_TABS.ALL:
             self._allSeasonAndTabFilterData[tabIndex] = {}
             for season in SeasonType.COMMON_SEASONS:
                 self._allSeasonAndTabFilterData[tabIndex][season] = CustomizationSeasonAndTypeFilterData()
 
-        for item in sorted(allItems.itervalues(), key=comparisonKey):
+        for item in sorted(allItems, key=comparisonKey):
             groupName = item.groupUserName
             for tabIndex in CUSTOMIZATION_TABS.ALL:
+                if tabIndex == CUSTOMIZATION_TABS.SHOP:
+                    if item.isHidden or item.priceGroup == 'modded':
+                        continue
+                if tabIndex == CUSTOMIZATION_TABS.HIDDEN:
+                    if not item.isHidden or os.path.splitext(os.path.basename(item.descriptor.texture))[0] in \
+                            g_config.interCamo or item.priceGroup == 'modded':
+                        continue
+                if tabIndex == CUSTOMIZATION_TABS.INTERNATIONAL:
+                    if os.path.splitext(os.path.basename(item.descriptor.texture))[0] not in g_config.interCamo:
+                        continue
+                if tabIndex == CUSTOMIZATION_TABS.CUSTOM:
+                    if item.priceGroup != 'modded':
+                        continue
                 for seasonType in SeasonType.COMMON_SEASONS:
                     if item.season & seasonType:
                         seasonAndTabData = self._allSeasonAndTabFilterData[tabIndex][seasonType]
