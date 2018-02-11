be19296b3cf64ee2fb572642c1f49ce8b14d95c0
PolyacovYury <kuf-1@yandex.ru>
2018-02-11 15:37:50 +0300

    Fix for HangarFreeCam

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
@@ -1,4 +1,16 @@
+from PYmodsCore import overrideMethod
+from gui.Scaleform.framework import ViewTypes
+from gui.app_loader import g_appLoader
 from gui.shared.gui_items.processors import Processor, makeI18nError
+from gui.ClientHangarSpace import _VehicleAppearance
+
+
+@overrideMethod(_VehicleAppearance, '_VehicleAppearance__getActiveOutfit')
+def new_getActiveOutfit(base, self):
+    c11nView = g_appLoader.getDefLobbyApp().containerManager.getContainer(ViewTypes.LOBBY_SUB).getView()
+    if c11nView is not None:
+        return c11nView.getCurrentOutfit()  # fix for HangarFreeCam
+    return base(self)
 
 
 class OutfitApplier(Processor):
