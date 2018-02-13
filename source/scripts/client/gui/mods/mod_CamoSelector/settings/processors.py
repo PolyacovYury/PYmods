81b385966f271a2b9e074853ae28a7f3d3f5b4ce
PolyacovYury <kuf-1@yandex.ru>
2018-02-13 19:27:10 +0300

    View getter fix

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
@@ -6,7 +6,9 @@ from gui.app_loader import g_appLoader
 
 @overrideMethod(_VehicleAppearance, '_VehicleAppearance__getActiveOutfit')
 def new_getActiveOutfit(base, self):
-    c11nView = g_appLoader.getDefLobbyApp().containerManager.getContainer(ViewTypes.LOBBY_SUB).getView()
-    if c11nView is not None:
-        return c11nView.getCurrentOutfit()  # fix for HangarFreeCam
+    container = g_appLoader.getDefLobbyApp().containerManager.getContainer(ViewTypes.LOBBY_SUB)
+    if container is not None:
+        c11nView = container.getView()
+        if c11nView is not None:
+            return c11nView.getCurrentOutfit()  # fix for HangarFreeCam
     return base(self)
