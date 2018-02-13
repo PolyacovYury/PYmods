248ebbe758c5a6e6b2766251121526324957209f
PolyacovYury <kuf-1@yandex.ru>
2018-02-13 19:50:25 +0300

    Container manager is also None sometines -_-

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/processors.py
@@ -6,9 +6,11 @@ from gui.app_loader import g_appLoader
 
 @overrideMethod(_VehicleAppearance, '_VehicleAppearance__getActiveOutfit')
 def new_getActiveOutfit(base, self):
-    container = g_appLoader.getDefLobbyApp().containerManager.getContainer(ViewTypes.LOBBY_SUB)
-    if container is not None:
-        c11nView = container.getView()
-        if c11nView is not None and hasattr(c11nView, 'getCurrentOutfit'):
-            return c11nView.getCurrentOutfit()  # fix for HangarFreeCam
+    manager = g_appLoader.getDefLobbyApp().containerManager
+    if manager is not None:
+        container = manager.getContainer(ViewTypes.LOBBY_SUB)
+        if container is not None:
+            c11nView = container.getView()
+            if c11nView is not None and hasattr(c11nView, 'getCurrentOutfit'):
+                return c11nView.getCurrentOutfit()  # fix for HangarFreeCam
     return base(self)
