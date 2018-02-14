5824287bdf5e837f2f56f0c8d56f69496fe0c071
PolyacovYury <kuf-1@yandex.ru>
2018-02-14 22:00:00 +0300

    Maybe?,,

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
@@ -1,10 +1,12 @@
 from config import g_config
+import anchor_properties
+import cm_handler
+import season_buttons_component
 from gui.Scaleform.framework import ScopeTemplates, ViewSettings, ViewTypes, g_entitiesFactories
-from . import anchor_properties, cm_handler, season_buttons_component
 from .main_view import CamoSelectorMainView
 
 __all__ = ['CamoSelectorMainView']
 
 # noinspection PyArgumentList
 g_entitiesFactories.addSettings(ViewSettings('CamoSelectorMainView', CamoSelectorMainView, 'customizationMainView.swf',
-                                ViewTypes.LOBBY_SUB, None, ScopeTemplates.LOBBY_SUB_SCOPE))
+                                             ViewTypes.LOBBY_SUB, None, ScopeTemplates.LOBBY_SUB_SCOPE))
