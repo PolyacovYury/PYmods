20efe2f0c296add03fb0b4d2aed99ddc383801db
PolyacovYury <kuf-1@yandex.ru>
2018-02-17 22:41:24 +0300

    Now season remap settings actually influence camouflages appearing in carousel. Also enabled season remapping for all camouflages

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/carousel.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/carousel.py
@@ -2,6 +2,7 @@ import nations
 from gui.Scaleform.daapi.view.lobby.customization.customization_carousel import CustomizationBookmarkVO, \
     CustomizationSeasonAndTypeFilterData, comparisonKey
 from gui.Scaleform.framework.entities.DAAPIDataProvider import SortableDAAPIDataProvider
+from gui.Scaleform.genConsts.SEASONS_CONSTANTS import SEASONS_CONSTANTS
 from gui.Scaleform.locale.VEHICLE_CUSTOMIZATION import VEHICLE_CUSTOMIZATION
 from gui.shared.utils.requesters import REQ_CRITERIA
 from helpers import dependency
@@ -13,9 +14,23 @@ from . import g_config
 from .shared import C11nTabs, isItemSuitableForTab
 
 
+def getItemSeason(item):
+    itemName, itemKey = (item.descriptor.userKey, 'custom') if item.priceGroup == 'custom' else (
+        item.id, 'remap')
+    itemSeason = item.season
+    if itemName in g_config.camouflages[itemKey]:
+        camoCfg = g_config.camouflages[itemKey][itemName]
+        if 'season' in camoCfg:
+            itemSeason = SeasonType.UNDEFINED
+            for season in SEASONS_CONSTANTS.SEASONS:
+                if season in camoCfg['season']:
+                    itemSeason |= getattr(SeasonType, season.upper())
+    return itemSeason
+
+
 def _createBaseRequirements(season=None):
     season = season or SeasonType.ALL
-    return REQ_CRITERIA.CUSTOM(lambda item: item.season & season)
+    return REQ_CRITERIA.CUSTOM(lambda item: getItemSeason(item) & season)
 
 
 class CustomizationCarouselDataProvider(SortableDAAPIDataProvider):
