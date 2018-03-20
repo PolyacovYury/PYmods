90aad1b1db661f63240f29113f41fb72ed2c7436
PolyacovYury <kuf-1@yandex.ru>
2018-03-20 10:53:33 +0300

    LampLigths: removing the light sources.

--- source/scripts/client/gui/mods/mod_LampLights.py
+++ source/scripts/client/gui/mods/mod_LampLights.py
@@ -33,7 +33,7 @@ class ConfigInterface(PYmodsCore.PYmodsConfigInterface):
 
     def init(self):
         self.ID = '%(mod_ID)s'
-        self.version = '2.2.2 (%(file_compile_date)s)'
+        self.version = '2.2.3 (%(file_compile_date)s)'
         self.defaultKeys = {'hotkey': [Keys.KEY_F12], 'hotKey': ['KEY_F12']}
         self.data = {'enabled': True,
                      'enableAtStartup': True,
@@ -619,6 +619,7 @@ def lightsDetach(vehicleID):
                 lightDBDict[vehicleID][confKey].visible = False
             else:
                 lightDBDict[vehicleID][confKey].source = None
+                lightVisible(lightDBDict[vehicleID][confKey], False)
 
 
 def lightsDestroy(vehicleID, callPlace=''):
