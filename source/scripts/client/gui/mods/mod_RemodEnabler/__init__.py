1c5dd33c7ab6e08bcc12325575c6258c23a560b6
PolyacovYury <kuf-1@yandex.ru>
2018-10-25 18:39:14 +0300

    RemodEnabler: Skinner needs to go first in terms of processors, but depends on g_config being present.

--- source/scripts/client/gui/mods/mod_RemodEnabler/__init__.py
+++ source/scripts/client/gui/mods/mod_RemodEnabler/__init__.py
@@ -1,12 +1,12 @@
 __modID__ = '%(mod_ID)s'
 __date__ = '%(file_compile_date)s'
 import PYmodsCore
+from config import g_config
 
 try:
     from gui.mods import mod_skinner
 except ImportError:
     pass
-from config import g_config
 import processor
 import collision
 
