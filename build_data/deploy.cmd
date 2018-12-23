e275c3b583955703173af3141628401ed3b6f389
PolyacovYury <kuf-1@yandex.ru>
2018-12-23 04:24:14 +0300

    Fixed deploy.cmd

--- build_data/deploy.cmd
+++ build_data/deploy.cmd
@@ -1,5 +1,5 @@
 @echo off
-start "" "C:\Program Files\Beyond Compare 4\BCompare.exe" archives\ "D:\Юрино\YandexDisk\PYmods\"
-start "" "C:\Program Files\Beyond Compare 4\BCompare.exe" archives_configs\ "D:\Юрино\YandexDisk\PYmods\"
+cd ..
+start "" "C:\Program Files\Beyond Compare 4\BCompare.exe" build\archives\ "D:\Юрино\YandexDisk\PYmods\"
 set /p ver=<build_data\GAME_VERSION.conf
 "C:\Program Files\Beyond Compare 4\BCompare.exe" build\wotmods\ "D:\Games\World_of_Tanks\mods\%ver%\" /solo
