222832c6178d9064d89b5d42fe6a6f53288ee7b5
PolyacovYury <kuf-1@yandex.ru>
2019-03-22 19:01:59 +0300

    Rewrote the getItemSeason method

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/shared.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/shared.py
@@ -4,6 +4,7 @@ from gui.shared.gui_items import GUI_ITEM_TYPE
 from items.components.c11n_constants import SeasonType
 from shared_utils import CONST_CONTAINER
 from .. import g_config
+from ..constants import SEASON_NAME_TO_TYPE
 
 
 class CSMode(CONST_CONTAINER):
@@ -43,14 +44,8 @@ class ACTION_ALIASES:
 
 
 def getItemSeason(item):
-    itemName, itemKey = (item.descriptor.userKey, 'custom') if item.priceGroup == 'custom' else (
-        item.id, 'remap')
-    itemSeason = item.season
-    if itemName in g_config.camouflages[itemKey]:
-        camoCfg = g_config.camouflages[itemKey][itemName]
-        if 'season' in camoCfg:
-            itemSeason = SeasonType.UNDEFINED
-            for season in SEASONS_CONSTANTS.SEASONS:
-                if season in camoCfg['season']:
-                    itemSeason |= getattr(SeasonType, season.upper())
-    return itemSeason
+    import operator
+    itemName, itemKey = (item.descriptor.userKey, 'custom') if item.priceGroup == 'custom' else (item.id, 'remap')
+    cfg = g_config.camouflages[itemKey].get(itemName, {})
+    seasons = cfg.get('season', []) or [x for x in SEASONS_CONSTANTS.SEASONS if SEASON_NAME_TO_TYPE[x] & item.season]
+    return reduce(operator.ior, (SEASON_NAME_TO_TYPE[x] for x in seasons), SeasonType.UNDEFINED)
