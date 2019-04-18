f55c22288b210534b159f2a18f4e4c496db18be1
PolyacovYury <kuf-1@yandex.ru>
2019-04-18 16:00:16 +0300

    Revert f1b446a "Skinner: process shared_content first. I'm tired of waiting for 10 minutes only to miss 5 seconds"

--- source/scripts/client/gui/mods/mod_Skinner/loader.py
+++ source/scripts/client/gui/mods/mod_Skinner/loader.py
@@ -303,7 +303,7 @@ def modelsProcess(callback):
     SoundGroups.g_instance.playSound2D(_WWISE_EVENTS.APPEAR)
     modelFileFormats = ('.model', '.visual', '.visual_processed', '.vt')
     print g_config.ID + ': unpacking vehicle packages'
-    for pkgPath in glob.glob('./res/packages/shared_content*.pkg') + glob.glob('./res/packages/vehicles*.pkg'):
+    for pkgPath in glob.glob('./res/packages/vehicles*.pkg') + glob.glob('./res/packages/shared_content*.pkg'):
         completionPercentage = 0
         callLoading('addBar', g_config.i18n['UI_loading_package'] % os.path.basename(pkgPath)[:-4].replace('sandbox', 'sb'))
         pkg = ZipFile(pkgPath)
