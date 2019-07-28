8e027ba2188d512df5c6a990b18fb2601e529c09
PolyacovYury <kuf-1@yandex.ru>
2019-07-28 20:55:54 +0300

    ...and to __init__

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
@@ -1,2 +1,2 @@
 from . import bottom_panel, carousel, cm_handlers, context, inscription_controller, installed_items_popover, main_view, \
-    properties_sheet, shared, tooltips
+    properties_sheet, shared, style_info, tooltips
