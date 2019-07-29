e8a2685792b3d5ce8f700f3866031a1610609662
PolyacovYury <kuf-1@yandex.ru>
2019-07-29 05:05:11 +0300

    CamoSelector: move all single-function class tweaks to a single file

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
@@ -1,2 +1,1 @@
-from . import bottom_panel, carousel, cm_handlers, context, inscription_controller, installed_items_popover, main_view, \
-    properties_sheet, shared, style_info, tooltips
+from . import bottom_panel, carousel, cm_handlers, context, properties_sheet, shared, tweaks
