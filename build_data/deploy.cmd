cf3dc20e7f9345cc964f3df23525e176c322dd8a
PolyacovYury <kuf-1@yandex.ru>
2019-09-19 20:55:21 +0300

    BCompare should be on PATH

--- build_data/deploy.cmd
+++ build_data/deploy.cmd
@@ -1,5 +1,5 @@
 @echo off
 cd ..
-start "" "C:\Program Files\Beyond Compare 4\BCompare.exe" build\archives\ "D:\Юрино\YandexDisk\PYmods\"
+start "" "BCompare.exe" build\archives\ "D:\Юрино\YandexDisk\PYmods\"
 set /p ver=<build_data\GAME_VERSION.conf
-"C:\Program Files\Beyond Compare 4\BCompare.exe" build\wotmods\ "D:\Games\World_of_Tanks\mods\%ver%\" /solo
+"BCompare.exe" build\wotmods\ "D:\Games\World_of_Tanks\mods\%ver%\" /solo
