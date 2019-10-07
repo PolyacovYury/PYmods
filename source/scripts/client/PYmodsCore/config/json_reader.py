3d715f7c376a1b5f2a755d7dc386a5ed592ff358
PolyacovYury <kuf-1@yandex.ru>
2019-10-07 23:56:28 +0300

    PYmodsCore: JSON object encoder no longer produces very long single-line lists

--- source/scripts/client/PYmodsCore/config/json_reader.py
+++ source/scripts/client/PYmodsCore/config/json_reader.py
@@ -17,11 +17,12 @@ class JSONObjectEncoder(json.JSONEncoder):
     def encode(self, o, i=0):
         try:  # Special Processing for lists
             if isinstance(o, (list, tuple)):
-                if not any(isinstance(x, dict) for x in o):
-                    return '[%s]' % ', '.join(self.encode(x) for x in o)
-                else:
-                    i += self.indent
-                    return '[\n%s\n%s]' % (',\n'.join((' ' * i + self.encode(x, i)) for x in o), ' ' * (i - self.indent))
+                if not any(isinstance(x, (dict, list, tuple)) for x in o):
+                    result = '[%s]' % ', '.join(self.encode(x) for x in o)
+                    if len(result) <= 128:
+                        return result
+                i += self.indent
+                return '[\n%s\n%s]' % (',\n'.join((' ' * i + self.encode(x, i)) for x in o), ' ' * (i - self.indent))
             elif isinstance(o, dict):
                 if not o:
                     return '{}'
