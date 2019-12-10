8ec23fb6800ade1f884c91f933b8ec2821bb5e5a
PolyacovYury <kuf-1@yandex.ru>
2019-12-10 18:34:10 +0300

    That's why start is needed there...

--- release.cmd
+++ release.cmd
@@ -1,8 +1,8 @@
 @echo off
 py -2 source/scripts/compile/packer.py -q -v build_data/GAME_VERSION.conf build_data/archives/ build/archives/
 echo Launching Beyond Compare...
-"BCompare.exe" build\archives\ "E:\Files\YandexDisk\PYmods\"
-"BCompare.exe" build\archives\ "E:\Files\GoogleDrive\PYmods\"
+start "" "BCompare.exe" build\archives\ "E:\Files\YandexDisk\PYmods\"
+start "" "BCompare.exe" build\archives\ "E:\Files\Google Drive\PYmods\"
 echo Exiting.
 pause
 exit
