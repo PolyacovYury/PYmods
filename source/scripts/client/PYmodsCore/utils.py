d89496c0522801ad0893c7f2d0df868d2f1bdff1
PolyacovYury <kuf-1@yandex.ru>
2020-03-19 05:33:45 +0300

    PYmodsCore: fix and refactor sendChatMessage

--- source/scripts/client/PYmodsCore/utils.py
+++ source/scripts/client/PYmodsCore/utils.py
@@ -118,13 +118,23 @@ def sendMessage(text='', colour='Green', panel='Player'):
 
 
 def sendChatMessage(fullMsg, chanId, delay):
-    currPart, remains = __splitMsg(fullMsg)
-    __sendMessagePart(currPart, chanId)
+    currPart, remains = __splitChatMessage(fullMsg)
+    __sendChatMessagePart(currPart, chanId)
     if remains:
-        BigWorld.callback(delay / 1000.0, partial(sendChatMessage, remains, chanId))
+        BigWorld.callback(delay / 1000.0, partial(sendChatMessage, remains, chanId, delay))
 
 
-def __sendMessagePart(msg, chanId):
+def __splitChatMessage(msg):
+    if len(msg) <= MAX_CHAT_MESSAGE_LENGTH:
+        return msg, ''
+    strPart = msg[:MAX_CHAT_MESSAGE_LENGTH]
+    splitPos = strPart.rfind(' ')
+    if splitPos == -1:
+        splitPos = MAX_CHAT_MESSAGE_LENGTH
+    return msg[:splitPos], msg[splitPos:]
+
+
+def __sendChatMessagePart(msg, chanId):
     import BattleReplay
     from messenger import MessengerEntry
     from messenger.m_constants import PROTO_TYPE
@@ -133,20 +143,10 @@ def __sendMessagePart(msg, chanId):
     proto = proto_getter(PROTO_TYPE.BW_CHAT2).get()
     if proto is None or BattleReplay.isPlaying():
         MessengerEntry.g_instance.gui.addClientMessage('OFFLINE: %s' % msg, True)
-    elif chanId in (0, 1):
+    elif chanId in (0, 1):  # 0 == 'All', 1 == 'Team'
         proto.arenaChat.broadcast(msg, int(not chanId))
     elif chanId == 2:
         proto.unitChat.broadcast(msg, 1)
-
-
-def __splitMsg(msg):
-    if len(msg) <= MAX_CHAT_MESSAGE_LENGTH:
-        return msg, ''
-    strPart = msg[:MAX_CHAT_MESSAGE_LENGTH]
-    splitPos = strPart.rfind(' ')
-    if splitPos == -1:
-        splitPos = MAX_CHAT_MESSAGE_LENGTH
-    return msg[:splitPos], msg[splitPos:]
 
 
 class Analytics(object):
