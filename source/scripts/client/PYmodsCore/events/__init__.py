4fe6c22c85db30911423808ac821aad9f30c36d3
PolyacovYury <kuf-1@yandex.ru>
2020-04-28 18:23:25 +0300

    PYmodsCore: allow all game.fini events to fire before clearing the event manager

--- source/scripts/client/PYmodsCore/events/__init__.py
+++ source/scripts/client/PYmodsCore/events/__init__.py
@@ -16,17 +16,19 @@ class ModEvent(object):
     def __init__(self):
         self.before = _EventWrapper()
         self.after = _EventWrapper()
+        self._final = _EventWrapper()
 
     def __call__(self, base, *args, **kwargs):
         self.before.event(*args, **kwargs)
         result = base(*args, **kwargs)
         self.after.event(*args, **kwargs)
+        self._final.event(*args, **kwargs)
         return result
 
 
 from . import game, PlayerAvatar, LobbyView, LoginView  # noqa: E402
 
 
-@game.fini.after
+@game.fini._final
 def fini(*_, **__):
     _manager.clear()
