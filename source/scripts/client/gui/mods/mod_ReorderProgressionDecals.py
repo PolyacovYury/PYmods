aece0568845b32066a229da81f57ead38aa262c1
PolyacovYury <kuf-1@yandex.ru>
2021-02-24 15:49:14 +0300

    ReorderProgressionDecals: progressive styles were added, we only work with decals, I'm sorry

--- source/scripts/client/gui/mods/mod_ReorderProgressionDecals.py
+++ source/scripts/client/gui/mods/mod_ReorderProgressionDecals.py
@@ -1,5 +1,6 @@
 from gui.impl.lobby.customization.progressive_items_view.progressive_items_view import ProgressiveItemsView
 from items import vehicles
+from items.components.c11n_constants import CustomizationType
 
 order = ('battles', 'frags', 'ranked', 'marksOfMastery', 'mainGun', 'BrothersInArms', )
 
@@ -7,8 +8,11 @@ order = ('battles', 'frags', 'ranked', 'marksOfMastery', 'mainGun', 'BrothersInA
 def _getPossibleItemsForVehicle(self):
     customizationCache = vehicles.g_cache.customization20()
     vehicleType = self._vehicle.descriptor.type
-    sortedItems = sorted(customizationCache.customizationWithProgression.itervalues(), key=lambda i: (
-        next((j for j, tag in enumerate(order) if tag in i.progression.levels[1][0]['path'][1]), 100), i.id
+    items = [item for item in customizationCache.customizationWithProgression.itervalues()
+             if item.itemType == CustomizationType.PROJECTION_DECAL]
+    sortedItems = sorted(items, key=lambda i: (
+        next((j for j, tag in enumerate(order) if tag in i.progression.levels[1][0]['path'][1]), 100),
+        i.id
     ))
     return [item.compactDescr for item in sortedItems if item.filter.matchVehicleType(vehicleType)]
 
