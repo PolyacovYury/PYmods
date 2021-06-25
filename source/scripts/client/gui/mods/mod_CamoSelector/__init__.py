e2d007295b02e81744cf0899a725b0a50700eb2a
PolyacovYury <kuf-1@yandex.ru>
2021-06-25 23:16:11 +0300

    auto-disable if something went wrong while importing

--- source/scripts/client/gui/mods/mod_CamoSelector/__init__.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/__init__.py
@@ -2,4 +2,8 @@
 __modID__ = '%(mod_ID)s'
 __date__ = '%(file_compile_date)s'
 from .config import g_config
-from . import processors, readers, settings
+try:
+    from . import processors, readers, settings
+except Exception:
+    g_config.data['enabled'] = False
+    raise
