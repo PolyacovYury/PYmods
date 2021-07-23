dc132809f47b0eb3d7afdec16d4327e9fc3da8c8
PolyacovYury <kuf-1@yandex.ru>
2021-07-23 12:31:44 +0300

    I need to know the current version to deliver working product

--- source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/settings/__init__.py
@@ -1,36 +1,39 @@
 # noinspection SpellCheckingInspection
 def a():
-    iI1I1Ii1iI = dict([
-        (iI1I1Ii1iI[0] ^ 161, ''.join(map(chr, [
-            int((bin(iI1I1Ii1iI)[2:].rjust(8, "0"))[::-1], 2) for iI1I1Ii1iI in iI1I1Ii1iI[1]])))
-        for iI1I1Ii1iI in __import__('marshal').loads(
-            'eNqFky9PxEAUxNuS8CdcCoSkQSEvBFGB4BuAQJxD8QHIIRBw6QdAoAgCcXhIKpFnUCAqLqSS4AjBnyCXE1XctNnf8sgRzGR2'
-            '+2be7rzt0VoQBFvRFLr3c1O2IHYhKICJ4EGQCVIU11LEYpf/KrqVoBQkKDqCXNDG9EmmLbHY1pUUn8D8hz59M6zWZZXQ'
-            '/FmwI7gR3AnOBNuUDPlwQCO/rGbsDfH7cn5N8ys1n6+ZoAd7Ydmi'
-            '+D10l97nHilQLwecwOeSEcQuVoeMsTCRNFZ18aYd40fkFDEKz05N2I1iRcddpa5jZX4yKZPJGdSvd5AD/kDnkYuuNhhRUl'
-            '/61Rk0xW8qXrRZTVwkP04QW9mx2NKMZ+YV6V/vak8Rh2Jj9jYit9dj75MeA3r0mUdFuj6hEdpH+S0zRj/9xE6/zVXtD9G43IYunMLe0v+hpXlD3y'
-            'f+qy8='.decode('base64').decode('zlib'))])
-    I1II1 = __import__(iI1I1Ii1iI[770])
-    iiiI1i = __import__(iI1I1Ii1iI[815])
-    # # # TODO: unpaid version should always execute, paid is under if
-    if True or getattr(iiiI1i, iI1I1Ii1iI[864])(getattr(I1II1, iI1I1Ii1iI[946])[1])[1:] == (iI1I1Ii1iI[813], 1):
-        for iIII in (
-                iI1I1Ii1iI[382], iI1I1Ii1iI[310], iI1I1Ii1iI[538], iI1I1Ii1iI[1012], iI1I1Ii1iI[66], iI1I1Ii1iI[576],
-                iI1I1Ii1iI[430], iI1I1Ii1iI[722],
-                iI1I1Ii1iI[633]):
-            __import__(getattr(__name__, iI1I1Ii1iI[198])(iI1I1Ii1iI[996])[0] + iI1I1Ii1iI[697] + iIII)
+    i11i1i1I11i = dict(
+        [(i11i1i1I11i[0] ^ 83, ''.join(map(chr, [
+            int((bin(i11i1i1I11i)[2:].rjust(8, "0"))[::-1], 2) for i11i1i1I11i in i11i1i1I11i[1]])))
+         for i11i1i1I11i in __import__('marshal').loads(
+            'eNqFUz1Lw1AUTdriB5YgDh0dSxHJ4NCf4OCQP6B/oB0cSnng2qFTB3FwcJSS0TGTIDhkCKWjs+'
+            'DgIh1E3hBEzHmPdx63DXQ5nCT3nnvvuTeXR0EQ9BoVDLpgu2ATQE7QgCeAAsTM+AWLwKZbMwYl'
+            'YAnoMCMBpCxuRP8q7HWY8Qo4A9wBHgEjwAlDCn64AAzlY1nzrqDet9OzxW/AdsBmgDHZgo9tBi'
+            'saFnFoz645W8KM54aTN5asGGLceHMO2eAPBO9T1Btmcu8l02zI5M7DioVgP3z3Ar1Temr0MuYW'
+            '7KVPlrOGZvErQIt1ldx5Jtv1/Z1L5damQ/Y25DA2rsu4uKY1no9pzY46bbrxx3z3gPH35ESaXf'
+            'lOI7mFd0gd8kMiN53WLCXlMGsnn4o5rPxt4Jac0TVvbEmbvPKKuYvQ3V8uXfO+HMtqc3hwwIyM'
+            'NfwvqWhnf3M9VuWz6ZzM5WH6334prsmmmfW0eQyancbyfEp5Q6pG6otSa7+Grzvcelz/Uz7XJQ=='.decode('base64').decode('zlib'))])
+    iiI1II = __import__(i11i1i1I11i[117])
+    iiIi111i1iII = __import__(i11i1i1I11i[175])
+    # # TODO: unpaid version should always execute, paid is under if
+    iIiii1I = getattr(iiIi111i1iII, i11i1i1I11i[191])(getattr(iiI1II, i11i1i1I11i[685])[1])[1:]
+    if True or iIiii1I == (i11i1i1I11i[42], 1):
+        for I111i1 in (
+                i11i1i1I11i[15], i11i1i1I11i[948], i11i1i1I11i[759], i11i1i1I11i[415], i11i1i1I11i[195],
+                i11i1i1I11i[37], i11i1i1I11i[947], i11i1i1I11i[751], i11i1i1I11i[713]):
+            __import__(getattr(__name__, i11i1i1I11i[688])[i11i1i1I11i[503]][0] + i11i1i1I11i[981] + I111i1)
     else:
-        print 'CamoSelector: incompatible version detected'
+        print i11i1i1I11i[748], iIiii1I
 
-# import Account
-# import account_shared
-
-# # # TODO: unpaid version should always execute, paid is under if
-# if account_shared.parseVersion(Account._CLIENT_SERVER_VERSION[1])[1:] == ('1.13.0', 1):
-#     for name in (
-#             'bottom_panel', 'carousel', 'cm_handlers', 'context', 'main_view', 'popover', 'properties_sheet', 'tweaks',
-#             'tooltips'):
-#         __import__(__name__.partition['/'] + '.' + name)
+    # import Account
+    # import account_shared
+    # # # TODO: unpaid version should always execute, paid is under if
+    # version = account_shared.parseVersion(Account._CLIENT_SERVER_VERSION[1])[1:]
+    # if version == ('1.13.0', 1):
+    #     for name in (
+    #             'bottom_panel', 'carousel', 'cm_handlers', 'context', 'main_view',
+    #             'popover', 'properties_sheet', 'tweaks', 'tooltips'):
+    #         __import__(__name__.partition['/'][0] + '.' + name)
+    # else:
+    #     print 'CamoSelector: incompatible version detected:', version
 
 
 a()
