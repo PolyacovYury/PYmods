a65665894ca5cee651b0410451d17bef8b34ca9a
PolyacovYury <kuf-1@yandex.ru>
2021-09-28 19:11:37 +0300

    future-proof fix for events

--- source/scripts/client/gui/mods/mod_CamoSelector/processors.py
+++ source/scripts/client/gui/mods/mod_CamoSelector/processors.py
@@ -297,8 +297,9 @@ def applyOutfitInfo(outfit, seasonName, vDesc, randomCache, isPlayerVehicle=True
 @overrideMethod(_PlatoonTankAppearance, '_getActiveOutfit')
 def new_getActiveOutfit(base, self, vDesc):
     outfit = base(self, vDesc)
-    if (not g_config.data['enabled'] or not vDesc or vDesc.name in g_config.disabledVehicles or (
-            vDesc.type.hasCustomDefaultCamouflage and g_config.data['disableWithDefault'])):
+    if (not g_config.data['enabled'] or not vDesc or vDesc.name in g_config.disabledVehicles
+            or not vDesc.turret.customizableVehicleAreas['camouflage']  # can be empty on event vehicles, everything breaks
+            or (vDesc.type.hasCustomDefaultCamouflage and g_config.data['disableWithDefault'])):
         return outfit
     nation, vehicleName = vDesc.name.split(':')
     if isinstance(self, _HeroTankAppearance):
@@ -344,8 +345,9 @@ def new_prepareOutfit(base, self, *a, **kw):
     vDesc = self.typeDescriptor
     if not vDesc:
         return outfit
-    if not g_config.data['enabled'] or vDesc.name in g_config.disabledVehicles or (
-            vDesc.type.hasCustomDefaultCamouflage and g_config.data['disableWithDefault']):
+    if (not g_config.data['enabled'] or vDesc.name in g_config.disabledVehicles
+            or not vDesc.turret.customizableVehicleAreas['camouflage']  # can be empty on event vehicles, everything breaks
+            or (vDesc.type.hasCustomDefaultCamouflage and g_config.data['disableWithDefault'])):
         return outfit
     if not g_config.data['useBought']:
         outfit = createEmptyOutfit(vDesc)
