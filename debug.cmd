44744b0303d635f162d5868062d143f2380709d3
PolyacovYury <kuf-1@yandex.ru>
2021-10-08 14:27:20 +0300

    moved build tools into submodule

--- debug.cmd
+++ debug.cmd
@@ -1,10 +1,1 @@
-@echo off
-:::git checkout develop
-py -2 source/scripts/compile/compiler.py -p PJOrion/PJOrion -d scripts/client/ -o build/scripts/client/ source/scripts/client/
-py -2 source/scripts/compile/packer.py -q -v build_data/GAME_VERSION build_data/wotmods/ build/wotmods/
-echo Launching Beyond Compare...
-set /p ver=<build_data\GAME_VERSION
-"BCompare.exe" build\wotmods\ "E:\Games\World_of_Tanks\mods\%ver%\" /solo
-echo Exiting.
-pause
-exit
+start build_tools/debug.cmd
--- release.cmd
+++ release.cmd
@@ -1,8 +1,1 @@
-@echo off
-py -2 source/scripts/compile/packer.py -q -v build_data/GAME_VERSION build_data/archives/ build/archives/
-echo Launching Beyond Compare...
-start "" "BCompare.exe" build\archives\ "E:\Files\YandexDisk\PYmods\"
-start "" "BCompare.exe" build\archives\ "E:\Files\Google Drive\PYmods\"
-echo Exiting.
-pause
-exit
+call build_tools/release.cmd
